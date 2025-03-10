f70ace7953d06e29bffa6d60ab5b6d0711db84a4
Nest <vlad.nesterenkov@gmail.com>
2025-03-10 20:05:08 +0100

    final

---
+++ src/hooks/useProfile.ts
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query'
+
+import { userService } from '@/services/studio/user.service'
+
+export function useProfile() {
+	const { data, isLoading, isSuccess, refetch } = useQuery({
+		queryKey: ['profile'],
+		queryFn: () => userService.getProfile(),
+		refetchInterval: 1800000 //30 min.
+	})
+
+	return {
+		profile: data?.data,
+		isLoading,
+		isSuccess,
+		refetch
+	}
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+import { COLORS } from './src/constants/colors.constants'
+
+const config: Config = {
+	content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			colors: COLORS,
+			padding: {
+				layout: '1.4rem'
+			},
+			transitionTimingFunction: {
+				DEFAULT: 'ease-in-out'
+			},
+			transitionDuration: {
+				DEFAULT: '333ms'
+			}
+		}
+	}
+}
+export default config
